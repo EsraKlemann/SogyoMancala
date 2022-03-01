dafa35bd049d9172d8934daa387fa240edf4f08c
Esra Klemann <eklemann@sogyo.nl>
2022-03-01 11:35:49 +0100

    failing test for opdr 4-6 CI/CD

---
+++ domain/src/test/java/mancala/domain/MancalaImplTest.java
@@ -0,0 +1,16 @@
+package mancala.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MancalaImplTest {
+
+    @Test
+    public void testHasToFail() {
+        int fouteInt = 5;
+        assertEquals(1, fouteInt);
+    }
+
+    
+}
