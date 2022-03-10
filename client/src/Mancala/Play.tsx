fa2f81e2ec54d67b1395da290e63b123c4d4ec07
Esra <eklemann@sogyo.nl>
2022-03-10 17:19:29 +0100

    Shows who has turn

--- client/src/Mancala/Play.tsx
+++ client/src/Mancala/Play.tsx
@@ -50,30 +50,40 @@ export function Play({ gameState, setGameState }: PlayProps) {
     endGame = "Game finished!"
   }
 
+  let atTurn = "";
+    if (player1.hasTurn) {
+    atTurn = player1.name + " chooses a bowl to play"
+    } if (player2.hasTurn) {
+        atTurn = player2.name + " chooses a bowl to play"
+    } if (gameState.gameStatus.endOfGame) {
+        atTurn = ""
+    }
+
 
   return (
-        <div className="game">
-            <p>{endGame}</p>
-            <p>{winner}</p>
-            <div className="namep2"><div className="textp2">{player2.name}</div></div>
-            <div className="board">
-                <div className="kalaha" id="kalahap2">{player2Kalaha.nrOfStones}</div>
-                <div className="row-wrapper">
-                  <div className="topboard">
-                    {player2.pits.slice(0,-1).reverse().map(pit =>
-                        <button className="bowl" id="bowlsp2" onClick={()=> { if(player2.hasTurn)tryMakeMove(pit.index)}}>{pit.nrOfStones}</button>
-                    )}
-                  </div>
-                  <div className="bottomboard">
-                    {player1.pits.slice(0,-1).map(pit =>
-                        <button className="bowl" id="bowlsp1" onClick={()=> { if(player1.hasTurn) tryMakeMove(pit.index)}}>{pit.nrOfStones}</button>
-                    )}
-                  </div>
+    <div className="game">
+        <p>{endGame}</p>
+        <p>{winner}</p>
+        <p>{atTurn}</p>
+        <div className="namep2"><div className="textp2">{player2.name}</div></div>
+        <div className="board">
+            <div className="kalaha" id="kalahap2">{player2Kalaha.nrOfStones}</div>
+            <div className="row-wrapper">
+                <div className="topboard">
+                {player2.pits.slice(0,-1).reverse().map(pit =>
+                    <button className="bowl" id="bowlsp2" onClick={()=> { if(player2.hasTurn)tryMakeMove(pit.index)}}>{pit.nrOfStones}</button>
+                )}
                 </div>
-                <div className="kalaha" id="kalahap1">{player1Kalaha.nrOfStones}</div>
+                <div className="bottomboard">
+                {player1.pits.slice(0,-1).map(pit =>
+                    <button className="bowl" id="bowlsp1" onClick={()=> { if(player1.hasTurn) tryMakeMove(pit.index)}}>{pit.nrOfStones}</button>
+                )}
+                </div>
             </div>
-            <div className="namep1"><div className="textp1">{player1.name}</div></div>
+            <div className="kalaha" id="kalahap1">{player1Kalaha.nrOfStones}</div>
         </div>
+        <div className="namep1"><div className="textp1">{player1.name}</div></div>
+    </div>
 
   );
 
